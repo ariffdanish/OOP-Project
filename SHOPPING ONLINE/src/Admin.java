8b17e8db53ecae740b97843c4ee8150bb2bb5d41
tiewrong <129280403+tiewrong@users.noreply.github.com>
2024-05-28 15:46:58 +0800

    commit

--- SHOPPING ONLINE/src/Admin.java
+++ SHOPPING ONLINE/src/Admin.java
@@ -1,3 +1,15 @@
 public class Admin{
+    private int aId;
+    private String aName;
+    private String aEmail;
+    private int aPhone;
+    private String password;
+    private Inventory inventory;
+
+    public Admin(){
+
+    }
+
     
+
 }
--- SHOPPING ONLINE/src/Payment.java
+++ SHOPPING ONLINE/src/Payment.java
@@ -1,3 +1,7 @@
 public class Payment {
+    private String paymentMethod;
+    private int amount;
+    private String paymentStatus;
+
     
 }
