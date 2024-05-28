0dcfabdad2a93a9aebaa1d692b30ed8f2c9e1d12
MuhammadImanFirdaus <muhammadimanfirdaus@graduate.utm.my>
2024-05-28 15:58:53 +0800

    Update shoppingCart.java

--- SHOPPING ONLINE/src/shoppingCart.java
+++ SHOPPING ONLINE/src/shoppingCart.java
@@ -1,20 +1,30 @@
 public class shoppingCart {
 
     private Product[] pList = new Product[25];
-    private float totalCost;
-    public t = 0;
+    private float totalCost = 0;
+    private int t = 0;
 
-    public void addProduct(Product p)
-    {
-        pList[t] = new Product(p);
-        
+    public void addProduct(Product p) {
+        if (t < pList.length) {
+            pList[t] = p;
+            totalCost += p.getPrice(); 
+            t++;
+        } else {
+            System.out.println("Cart is full!");
+        }
     }
 
-    public void rmvProduct(Product p)
-    {
-        for (int i = 0; i<t;i++)
-        delete plist[i] = p;
-        
+    public void rmvProduct(Product p) {
+        for (int i = 0; i < t; i++) {
+            if (pList[i].equals(p)) {
+                totalCost -= pList[i].getPrice(); 
+                for (int j = i; j < t - 1; j++) {
+                    pList[j] = pList[j + 1];
+                }
+                pList[t - 1] = null; 
+                t--;
+                break; 
+            }
+        }
     }
-    
 }
